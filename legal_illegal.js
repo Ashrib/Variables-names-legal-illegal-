0a22cc8a98ecec5b4e17c7ec9b4b89c933fc47a1
Asharib Ali <96954816+Ashrib@users.noreply.github.com>
2022-05-21 13:20:46 +0500

    fonts

---
+++ legal_illegal.js
@@ -0,0 +1,24 @@
+        // Legal & illegal variables
+// 1...
+var x="hello";
+var y="world";
+var z="!";
+alert(x +" "+ y +" "+ z);
+//2...(legal)
+var yourName="";
+var my_1stCar="";
+var $dollar="";
+var _underscore="";
+var hello_world="";
+//3...(illegal)
+// var 1stCar="";
+// var @email="";
+// var var="";
+// var percent%="";
+// var hash#="";
+//4...
+document.write("a. A heading stating “Rules for naming JS variables”");document.write("<br>");
+document.write("b. A variable name can contain only  ______, ______, ______ and ______."+"<br>"+"For example $my_1stVariable");document.write("<br>");
+document.write("c. Variables must begin with a ______, ______ or _____."+"<br>"+ "For example $name, _name or name");document.write("<br>");
+document.write("d. Variable names are case ________.");document.write("<br>");
+document.write("e. Variable names should not be JS ________.");document.write("<br>");
